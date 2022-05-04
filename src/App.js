e2912d37e564b33d7ba5faa86eeb9a4fb5d7d06e
Marcan <gallezmarcan@gmail.com>
2022-05-04 16:24:56 +0200

    Premier commit d'un simple input react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { actualisation: "Bonjour" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          name="actualisation"
+          value={this.state.actualisation}
+          type="text"
+          onChange={this.handleChange}
+        />
+        <p>{this.state.actualisation}</p>
+      </div>
+    );
+  }
 }
-
 export default App;
